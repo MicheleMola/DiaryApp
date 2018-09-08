1c62f4aa49efbfe7963d0cbbb3217f6b92deddb7
Michele Mola <michele.mola92@gmail.com>
2018-09-08 09:49:42 +0200

    Initial Commit

---
+++ DiaryApp/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  DiaryApp
+//
+//  Created by Michele Mola on 07/09/2018.
+//  Copyright © 2018 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail = detailItem {
+        if let label = detailDescriptionLabel {
+            label.text = detail.timestamp!.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  var detailItem: Event? {
+    didSet {
+        // Update the view.
+        configureView()
+    }
+  }
+
+
+}
+
