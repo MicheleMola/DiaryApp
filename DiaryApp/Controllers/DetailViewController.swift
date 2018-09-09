e81077465fb3f1f21b4f7168a0257c2a1ab8efb9
Michele Mola <michele.mola92@gmail.com>
2018-09-09 11:18:56 +0200

    Setup UI

--- DiaryApp/Controllers/DetailViewController.swift
+++ DiaryApp/Controllers/DetailViewController.swift
@@ -9,37 +9,34 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-  @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+  
   func configureView() {
     // Update the user interface for the detail item.
-//    if let detail = detailItem {
-//        if let label = detailDescriptionLabel {
-//            label.text = detail.timestamp!.description
-//        }
-//    }
+    //    if let detail = detailItem {
+    //        if let label = detailDescriptionLabel {
+    //            label.text = detail.timestamp!.description
+    //        }
+    //    }
   }
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     configureView()
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  
   var detailItem: Entry? {
     didSet {
-        // Update the view.
-        configureView()
+      // Update the view.
+      configureView()
     }
   }
-
-
+  
+  
 }
 
